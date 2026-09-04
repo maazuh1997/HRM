9e0431b61975f03124e3c1984c742edc0bda61f8
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 09:06:14 -0700

    feat: register outbox worker

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,7 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { DomainEventBus } from './domain-event.bus';
 import { OutboxService } from './outbox.service';
+import { OutboxWorker } from './outbox.worker';
 
 @Global()
-@Module({ providers: [DomainEventBus, OutboxService], exports: [DomainEventBus, OutboxService] })
+@Module({ providers: [DomainEventBus, OutboxService, OutboxWorker], exports: [DomainEventBus, OutboxService, OutboxWorker] })
 export class EventsModule {}
