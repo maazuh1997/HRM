920661d4b016c66bd0742c24d91e6e0eee6acba1
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 09:19:09 -0700

    feat: export notification factory

--- apps/api/src/notifications/notification.module.ts
+++ apps/api/src/notifications/notification.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { NotificationFactory } from './notification.factory';
 
-@Module({ providers: [NotificationService], exports: [NotificationService] })
+@Module({ providers: [NotificationService, NotificationFactory], exports: [NotificationService, NotificationFactory] })
 export class NotificationModule {}
