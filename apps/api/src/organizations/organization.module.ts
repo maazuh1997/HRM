d332e12b8ccb59dabc2c8aa0386758e59ca1b00d
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 07:19:45 -0700

    feat: wire complete organization invitation lifecycle

--- apps/api/src/organizations/organization.module.ts
+++ apps/api/src/organizations/organization.module.ts
@@ -4,11 +4,15 @@ import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
 import { TenantGuard } from './tenant.guard';
 import { RoleProvisioningService } from './role-provisioning.service';
+import { InvitationService } from './invitation.service';
+import { InvitationAcceptanceService } from './invitation.acceptance.service';
+import { InvitationController } from './invitation.controller';
+import { InvitationAcceptanceController } from './invitation.acceptance.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [OrganizationController],
-  providers: [OrganizationService, TenantGuard, RoleProvisioningService],
-  exports: [OrganizationService, TenantGuard, RoleProvisioningService],
+  controllers: [OrganizationController, InvitationController, InvitationAcceptanceController],
+  providers: [OrganizationService, TenantGuard, RoleProvisioningService, InvitationService, InvitationAcceptanceService],
+  exports: [OrganizationService, TenantGuard, RoleProvisioningService, InvitationService],
 })
 export class OrganizationModule {}
