013aa9e2301b81806e9395bb5a6ebdcfb767660f
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 07:05:21 -0700

    feat: register role provisioning in organization module

--- apps/api/src/organizations/organization.module.ts
+++ apps/api/src/organizations/organization.module.ts
@@ -3,11 +3,12 @@ import { AuthModule } from '../auth/auth.module';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
 import { TenantGuard } from './tenant.guard';
+import { RoleProvisioningService } from './role-provisioning.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [OrganizationController],
-  providers: [OrganizationService, TenantGuard],
-  exports: [OrganizationService, TenantGuard],
+  providers: [OrganizationService, TenantGuard, RoleProvisioningService],
+  exports: [OrganizationService, TenantGuard, RoleProvisioningService],
 })
 export class OrganizationModule {}
