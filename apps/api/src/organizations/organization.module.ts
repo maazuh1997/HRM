d2e2652b8d29ed84d41ccda5d5cd8bd27ba99512
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 06:35:53 -0700

    feat: register tenant guard

--- apps/api/src/organizations/organization.module.ts
+++ apps/api/src/organizations/organization.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
+import { TenantGuard } from './tenant.guard';
 
 @Module({
   imports: [AuthModule],
   controllers: [OrganizationController],
-  providers: [OrganizationService],
-  exports: [OrganizationService],
+  providers: [OrganizationService, TenantGuard],
+  exports: [OrganizationService, TenantGuard],
 })
 export class OrganizationModule {}
