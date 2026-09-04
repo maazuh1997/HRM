6b808193ab704877049e62ca0247c76f82878289
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 08:45:50 -0700

    fix: wire leave security dependencies

--- apps/api/src/leave/leave.module.ts
+++ apps/api/src/leave/leave.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { LeaveController } from './leave.controller';
 import { LeaveService } from './leave.service';
 import { ApprovalModule } from '../workflows/approval.module';
+import { OrganizationModule } from '../organizations/organization.module';
+import { AuthorizationModule } from '../authorization/authorization.module';
 
-@Module({ imports: [ApprovalModule], controllers: [LeaveController], providers: [LeaveService], exports: [LeaveService] })
+@Module({ imports: [ApprovalModule, OrganizationModule, AuthorizationModule], controllers: [LeaveController], providers: [LeaveService], exports: [LeaveService] })
 export class LeaveModule {}
