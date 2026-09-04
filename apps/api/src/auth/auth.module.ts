2aa9fcd209ff0d4f37d6b40e1b54c814c99aa7bd
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 06:31:40 -0700

    feat: register authentication guard

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 import { SessionService } from './session.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, SessionService],
-  exports: [AuthService, SessionService],
+  providers: [AuthService, AuthGuard, SessionService],
+  exports: [AuthService, AuthGuard, SessionService],
 })
 export class AuthModule {}
