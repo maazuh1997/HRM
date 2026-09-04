a43121c0e4c9d0bf1678d83fc8c637e5bf6aeb47
maazuh1997 <47713396+maazuh1997@users.noreply.github.com>
2026-09-04 09:41:24 -0700

    feat: register leave notification handler

--- apps/api/src/notifications/notification.module.ts
+++ apps/api/src/notifications/notification.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationFactory } from './notification.factory';
+import { LeaveNotificationHandler } from './leave-notification.handler';
 
-@Module({ providers: [NotificationService, NotificationFactory], exports: [NotificationService, NotificationFactory] })
+@Module({ providers: [NotificationService, NotificationFactory, LeaveNotificationHandler], exports: [NotificationService, NotificationFactory] })
 export class NotificationModule {}
